d735e8eb62c293dae015b315cc50e7977824ae02
waleed-cs <waleedalharthi1999@gmail.com>
2021-07-01 22:48:31 +0300

    video resize issue solved

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const videoPlayer = document.querySelector(".video");
 
 //fix the resize issue when the src changes
 
-videoPlayer.addEventListener("canplay", (e) => {
+videoPlayer.addEventListener("onloadeddata", (e) => {
   let player = e.target;
   player.width = player.clientWidth;
   player.height = player.clientHeight;
